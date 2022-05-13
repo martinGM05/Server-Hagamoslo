36cfba85368c70649c134d3708521289c2ac69f6
Martin Gonzalez Miranda <mtn.gm05@gmail.com>
2022-05-12 21:18:45 -0500

    TRYCATCH

--- lib/controller/AuthController.ts
+++ lib/controller/AuthController.ts
@@ -5,14 +5,18 @@ import UserService from "../service/UserService";
 export const authenticateUser = async (req: Request, res: Response) => {
     const { correo, contrasena } = req.body;
     try {
-        const user = await UserService.authenticateUser(correo, contrasena);
-        if(user){
-            res.status(200).json(user);
-        }else{
-            res.status(400).json({
-                message: 'Error al autenticar usuario'
-            });
-        }
+        await UserService.authenticateUser(correo, contrasena)
+            .then(user => {
+                res.status(200).json({
+                    user
+                });
+            })
+            .catch(err => {
+                res.status(500).json({
+                    err,
+                    msg: 'Hay pedo'
+                });
+            })
     } catch (error) {
         console.log(error)
         res.status(500).json({ message: 'Error al autenticar usuario' });
--- lib/service/UserService.ts
+++ lib/service/UserService.ts
@@ -5,22 +5,22 @@ import generateJWT from '../helpers/generate-jwt';
 const bcrypt = require('bcryptjs');
 const prisma = new PrismaClient()
 
-class UserService{
+class UserService {
 
-    static createUser = async(user: User) => {
+    static createUser = async (user: User) => {
         return prisma.usuario.create({
             data: {
                 ...user,
                 contrasena: bcrypt.hashSync(user.contrasena, 9)
             }
         })
-    }    
+    }
 
-    static getAllUsers(){
+    static getAllUsers() {
         return prisma.usuario.findMany()
     }
 
-    static getUserById(id: number){
+    static getUserById(id: number) {
         return prisma.usuario.findFirst({
             where: { id }
         }).then(user => {
@@ -38,12 +38,13 @@ class UserService{
         })
     }
 
-    static authenticateUser(correo: string, contrasena: string){
-        return prisma.usuario.findFirst({
+    static authenticateUser = async (correo: string, contrasena: string) => {
+
+        const user = await prisma.usuario.findFirst({
             where: { correo }
         }).then(async user => {
-            if(user){
-                if(bcrypt.compareSync(contrasena, user.contrasena)){
+            if (user) {
+                if (bcrypt.compareSync(contrasena, user.contrasena)) {
                     const userForToken = {
                         id: user.id,
                         correo: user.correo,
@@ -58,16 +59,39 @@ class UserService{
                         user,
                         token
                     }
-                }else{
-                    return null
                 }
             }
-        }).catch(err => {
-            return null
         })
+
+        // return prisma.usuario.findFirst({
+        //     where: { correo }
+        // }).then(async user => {
+        //     if(user){
+        //         if(bcrypt.compareSync(contrasena, user.contrasena)){
+        //             const userForToken = {
+        //                 id: user.id,
+        //                 correo: user.correo,
+        //                 nombre: user.nombre,
+        //                 urlFoto: user.urlFoto,
+        //                 numero: user.numero,
+        //                 idRol: user.idRol,
+        //             }
+        //             const token = await generateJWT(userForToken);
+
+        //             return {
+        //                 user,
+        //                 token
+        //             }
+        //         }else{
+        //             return null
+        //         }
+        //     }
+        // }).catch(err => {
+        //     return null
+        // })
     }
 
-    static changeRoleClient(idUser: number, idRol:number){
+    static changeRoleClient(idUser: number, idRol: number) {
 
         return prisma.usuario.update({
             where: { id: idUser },
@@ -80,14 +104,14 @@ class UserService{
 
     }
 
-    static putUser(id: number, user: User){
+    static putUser(id: number, user: User) {
         return prisma.usuario.update({
             where: { id: id },
             data: user
         })
     }
 
-    static getUserByRole(idRol: number){
+    static getUserByRole(idRol: number) {
         return prisma.usuario.findMany({
             where: { idRol }
         })
